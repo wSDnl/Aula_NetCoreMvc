f543909971ba8947c00b7570e5ed5e4312d612fe
TiagoCavalcante <atendimento@wssh.com.br>
2022-08-18 15:09:44 -0300

    #181509AGO22 Aula 30/185 Criando View para exibição dos dados

---
+++ NetCore6Mvc/Controllers/ProdutoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore6Mvc.Repositories.Interfaces;
+
+namespace NetCore6Mvc.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        private readonly IProdutoRepository _produtoRepositorio;
+
+        public ProdutoController(IProdutoRepository produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
+        public IActionResult List()
+        {
+
+            var produto = _produtoRepositorio.Produtos;
+
+            return View(produto);
+
+        }
+    }
+}
--- NetCore6Mvc/Models/Produto.cs
+++ NetCore6Mvc/Models/Produto.cs
@@ -11,41 +11,42 @@ namespace NetCore6Mvc.Models
         public int ProdutoId { get; set; }
 
         [Required(ErrorMessage ="[ERRO] Informe o nome do poduto")]
-        [Display(Name="Nome do Produto")]
+        [Display(Name="Produto")]
         [StringLength(100, MinimumLength = 10, ErrorMessage ="[ERRO] O {0} deve ter no mínimo {1} e no máxinmo {2} ")]
         public string Nome { get; set; }
         
         [Required(ErrorMessage ="[ERRO] Informe a descrição curta do produto")]
-        [Display(Name ="Descrição curta do Produto")]
+        [Display(Name ="Descrição Curta")]
         [StringLength(100, MinimumLength = 10 , ErrorMessage ="[ERRO] O {0} deve ter no mínimo {1} e no máximo {2} ")]
         public string DescricaoCurta { get; set; }
 
         [Required(ErrorMessage ="[ERRO] Informe a descrição detalhada do produto")]
-        [Display(Name ="Descrição detalhada do Produto")]
+        [Display(Name ="Descrição Detalhada")]
         [StringLength(100, MinimumLength = 10 , ErrorMessage ="[ERRO] O {0} deve ter no mínimo {1} e no máximo {2} ")]
         public string DescricaoDetalhada { get; set; }
 
         [Required(ErrorMessage ="[ERRO] Informe o valor do produto")]
-        [Display(Name ="Preço")]
+        [Display(Name ="R$")]
         [Column(TypeName ="decimal(10,2)")]
         [Range(1,999.99,ErrorMessage ="O valor de estar entre 1 e 999,99")]
         public decimal Valor { get; set; }
 
-
+        [Display(Name = "Url Imagem")]
         public string ImagemUrl { get; set; }
 
-
+        [Display(Name = "Url Imagem Mini")]
         public string ImagemMini { get; set; }
 
-
+        [Display(Name = "Disponível")]
         public bool Disponivel { get; set; }
 
         [Required]
-        [Display(Name ="Quantidade do produto em estoque")]
+        [Display(Name ="Quantidade")]
         public string QtdeEstoque { get; set; }
 
 
         [Required(ErrorMessage ="[ERRO] Defina a categoria do produto")]
+        [Display(Name = "Categoria")]
         public int CategoriaId { get; set; }
 
 
