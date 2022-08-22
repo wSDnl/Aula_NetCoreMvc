be9d1e810c6068792a8a3dca41503274962e22fc
TiagoCavalcante <atendimento@wssh.com.br>
2022-08-22 14:28:25 -0300

    #221420AGO22 Aula 45/185 Método AdicionaAoCarrinho.cs

--- NetCore6Mvc/Models/CarrinhoCompra.cs
+++ NetCore6Mvc/Models/CarrinhoCompra.cs
@@ -24,34 +24,63 @@ namespace NetCore6Mvc.Models
 
 
 
+				public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+				{
+					// DEFINE UMA SESSÃO
+					ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
 
 
-		public static CarrinhoCompra GetCarrinho(IServiceProvider services)
-		{
-			// DEFINE UMA SESSÃO
-			ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+					// OBTEM UM SERVIÇO DO TIPO DO NOSSO CONTEXTO
+					var context = services.GetService<AppDbContext>();
 
 
-			// OBTEM UM SERVIÇO DO TIPO DO NOSSO CONTEXTO
-			var context = services.GetService<AppDbContext>();
+					// OBTER OU GERAR O ID DO CARRINHO
+					string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
 
 
-			// OBTER OU GERAR O ID DO CARRINHO
-			string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+					// ATRIBUI O ID DO CARRINHO NA SESSÃO
+					session.SetString("CarrinhoId", carrinhoId);
 
 
-			// ATRIBUI O ID DO CARRINHO NA SESSÃO
-			session.SetString("CarrinhoId", carrinhoId);
+					return new CarrinhoCompra(context)
+					{
+						CarrinhoCompraId = carrinhoId
+					};
+
+				}
+
+        /// *** METODOS *** ///
 
 
-			return new CarrinhoCompra(context)
-			{
-				CarrinhoCompraId = carrinhoId
-			};
+				public void AdicionarAoCarrinho(Produto produto)
+				{
+					var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
+						s=>	s.Produto.ProdutoId == produto.ProdutoId &&
+						s.CarrinhoCompraId == CarrinhoCompraId
+					);
 
-		}
+
+						if (carrinhoCompraItem == null)
+						{
+							carrinhoCompraItem = new CarrinhoCompraItem
+							{
+								CarrinhoCompraId = CarrinhoCompraId,
+								Produto = produto,
+								Quantidade = 1,
+							};
+
+							_context.CarrinhoCompraItens.Add(carrinhoCompraItem);
+
+						} else {
+							carrinhoCompraItem.Quantidade++;
+						}
+
+						_context.SaveChanges();
+
+				}
 
 
+        /// *** /METODOS *** ///
 
 	}
 }
