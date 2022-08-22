2fc4f0f61b3892416c959f34536be254fc4abe18
TiagoCavalcante <atendimento@wssh.com.br>
2022-08-22 14:17:04 -0300

    #221415AGO22 Aula 42/185 Criando classe CarrinhoCompras.cs

---
+++ NetCore6Mvc/Models/CarrinhoCompra.cs
@@ -0,0 +1,57 @@
+﻿using NetCore6Mvc.Context;
+
+namespace NetCore6Mvc.Models
+{
+	public class CarrinhoCompra
+	{
+
+
+
+
+		private readonly AppDbContext _context;
+
+		public CarrinhoCompra(AppDbContext context)
+		{
+			_context = context;
+		}
+
+
+
+
+		public string CarrinhoCompraId { get; set; }
+
+		public List<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+
+
+
+
+
+		public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+		{
+			// DEFINE UMA SESSÃO
+			ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+
+			// OBTEM UM SERVIÇO DO TIPO DO NOSSO CONTEXTO
+			var context = services.GetService<AppDbContext>();
+
+
+			// OBTER OU GERAR O ID DO CARRINHO
+			string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+
+			// ATRIBUI O ID DO CARRINHO NA SESSÃO
+			session.SetString("CarrinhoId", carrinhoId);
+
+
+			return new CarrinhoCompra(context)
+			{
+				CarrinhoCompraId = carrinhoId
+			};
+
+		}
+
+
+
+	}
+}
