2dd1902ccb45d6a691a8938950cfb8c928ea9c87
TiagoCavalcante <atendimento@wssh.com.br>
2022-08-22 16:50:46 -0300

    #221650AGO22 Aula 49/185 Fim CarrinhoCompraController

--- NetCore6Mvc/Controllers/CarrinhoCompraController.cs
+++ NetCore6Mvc/Controllers/CarrinhoCompraController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NetCore6Mvc.Models;
 using NetCore6Mvc.Repositories.Interfaces;
+using NetCore6Mvc.ViewModels;
 
 namespace NetCore6Mvc.Controllers
 {
@@ -18,7 +19,42 @@ namespace NetCore6Mvc.Controllers
 
 		public IActionResult Index()
 		{
-			return View();
+			var itens = _carrinhoCompraController.GetCarrinhoCompraItens();	
+			_carrinhoCompraController.CarrinhoCompraItens = itens;
+
+			var carrinhoCompraVM = new CarrinhoCompraViewModel
+			{
+				CarrinhoCompra = _carrinhoCompraController,
+				CarrinhoCompraTotal = _carrinhoCompraController.GetCarrinhoCompraTotal()
+			};
+
+			return View(carrinhoCompraVM);
 		}
+
+		public IActionResult AdicionarItemNoCarrinhoCompra(int produtoId)
+		{
+			var produtoSelecionado = _produtoRepository.Produtos.FirstOrDefault(p => p.ProdutoId == produtoId);
+			if (produtoSelecionado != null)
+			{
+				_carrinhoCompraController.AdicionarAoCarrinho(produtoSelecionado);
+			}
+
+		return RedirectToAction("Index");
+
+		}
+
+
+		public IActionResult RemoverItemDoCarrinhoCompra(int produtoId)
+		{
+            var produtoSelecionado = _produtoRepository.Produtos.FirstOrDefault(p => p.ProdutoId == produtoId);
+            if (produtoSelecionado != null)
+            {
+				_carrinhoCompraController.RemoverDoCarrinho(produtoSelecionado);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
 	}
 }
