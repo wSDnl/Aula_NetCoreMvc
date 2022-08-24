d82dbc2fb670858adacfd3e6fae1f0b17e9d9753
wSDnl <84946481+wSDnl@users.noreply.github.com>
2022-08-23 23:13:05 -0300

    #teste

---
+++ NetCore6Mvc/Migrations/20220823173620_InsertCategorias.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetCore6Mvc.Migrations
+{
+    public partial class InsertCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome,Descricao)" +
+                "VALUES('Normal','Produto Normal')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome,Descricao)" +
+                "VALUES('Importados','Produto Importado')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
